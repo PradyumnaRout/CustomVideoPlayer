119719d476ce1446b572f3aa02ef7a315ef8d195
Pradyumna Rout <pradyumnarout1433@gmail.com>
2025-09-10 18:12:06 +0530

    custom buttons for player

--- CustomVideoPlayer/CustomVideoPlayerApp.swift
+++ CustomVideoPlayer/CustomVideoPlayerApp.swift
@@ -5,10 +5,6 @@
 //  Created by hb on 10/09/25.
 //
 
-// Ref - video player - https://medium.com/@tokusha.aa/mastering-swiftui-and-avplayer-integration-a-complete-guide-to-timecodes-and-advanced-playback-6ef9a88b3b8d
-
-// https://www.createwithswift.com/custom-video-player-with-avkit-and-swiftui-supporting-picture-in-picture/
-
 
 import SwiftUI
 
