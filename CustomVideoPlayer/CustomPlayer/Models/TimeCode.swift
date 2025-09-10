ab19df6be189953b77d8bdd6de28d1ecddd61309
Pradyumna Rout <pradyumnarout1433@gmail.com>
2025-09-10 18:09:01 +0530

    Initial Commit

---
+++ CustomVideoPlayer/CustomPlayer/Models/TimeCode.swift
@@ -0,0 +1,16 @@
+//
+//  TimeCode.swift
+//  CustomVideoPlayer
+//
+//  Created by hb on 10/09/25.
+//
+
+import SwiftUI
+import AVKit
+
+
+// Time code
+struct Timecode {
+    let title: String
+    let time: CMTime
+}
---
+++ CustomVideoPlayer/CustomVideoPlayerApp.swift
@@ -0,0 +1,22 @@
+//
+//  CustomVideoPlayerApp.swift
+//  CustomVideoPlayer
+//
+//  Created by hb on 10/09/25.
+//
+
+// Ref - video player - https://medium.com/@tokusha.aa/mastering-swiftui-and-avplayer-integration-a-complete-guide-to-timecodes-and-advanced-playback-6ef9a88b3b8d
+
+// https://www.createwithswift.com/custom-video-player-with-avkit-and-swiftui-supporting-picture-in-picture/
+
+
+import SwiftUI
+
+@main
+struct CustomVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
